19f06d1bbf59e3f891cb170bf81d359ae4200e13
Robson Vasconcellos <rrvasconcellos@outlook.com>
2021-08-25 13:43:40 -0300

    Avançando Json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,17 +8,40 @@ const App = () => {
 
   const [state, setState] = useState("OIOIODJIAHODIAS")
 
-  const clique = function() {
-    fetch('/data/eventTypes.json').then(res => res.json())
-      .then(res => (<div><pre> {
-        setState(JSON.stringify(res.injection))
-      })</pre></div>))
+  const criarOnClick = function(campo) {
+
+    return function() {
+      console.log(campo);
+      fetch('/data/eventTypes.json').then(res => res.json())
+        .then(res => {
+          setState(JSON.stringify(res[campo]))
+        })
+    }
   }
+
 
   return (
     <AppLayout>
       <AppLayout.Left>
-        <NavItem selected={false} onClick={clique}>Bounce</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('bounce')}>Bounce</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('delivery')}>Delivery</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('injection')}>Injection</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('sms_status')}>SMS Status</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('spam_complaint')}>Spam Complaint</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('out_of_band')}>Out of Band</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('policy_rejection')}>Policy Rejection</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('delay')}>Delay</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('click')}>Click</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('open')}>Open</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('initial_open')}>Initial Open</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('amp_click')}>AMP Click</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('amp_open')}>AMP Open</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('amp_initial_open')}>AMP Initial Open</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('generation_failure')}>Generation Failure</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('generation_rejection')}>Generation Rejection</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('list_unsubscribe')}>List Unsubscribe</NavItem>
+        <NavItem selected={false} onClick={criarOnClick('link_unsubscribe')}>Link Unsubscribe</NavItem>
+        
       </AppLayout.Left>
       <AppLayout.Right>{state}</AppLayout.Right>
     </AppLayout>
